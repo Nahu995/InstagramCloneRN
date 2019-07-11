b223ea90947d40f2f7246cb17454d988b5ec83f9
nahu995 <nahu995@gmail.com>
2019-07-11 10:02:21 -0300

    Sagas

--- Store/Sagas/Sagas.js
+++ Store/Sagas/Sagas.js
@@ -3,21 +3,18 @@ import { autentication } from '../Services/Firebase'
 const firebaseRegistration =  (values) => (
   autentication
   .createUserWithEmailAndPassword(values.email, values.password)
-    .then((success) => {
-      console.log("success \n", success)
-    })
-    .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode)
-      console.log(errorMessage)
-    })
+    .then(success => success)
+    .catch(error => error)
 );
 
 
 function* generateRegister (values) {
-  yield call( firebaseRegistration, values.data)
-  console.log("generateRegister \n",values,"\n generateRegister ")
+  try{
+    const register = yield call( firebaseRegistration, values.data)
+    console.log("REGISTER =>" , register)
+  }catch(error){
+    console.log(error)
+  }
 }
 
 export default function* functionPrimaria () {
